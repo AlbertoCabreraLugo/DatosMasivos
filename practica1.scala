8f3d26e9f9dc1b1e5fe2a2111b0c0c2f192c52e6
Alberto Cabrera Lugo <alberto.cabrera.lugo@gmail.com>
2020-10-13 11:57:37 -0700

    Primeros ejercicios de la practica 1 y 2

---
+++ practica1.scala
@@ -0,0 +1,13 @@
+//4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slice para extraer la
+//   secuencia "Luke"
+val saludo = ("Hola Luke yo soy tu padre!")
+saludo slice (5,9)
+
+//5. Cual es la diferencia entre value y una variable en scala
+//Que la variable es mutable y en scala no es mutable te genera un error
+
+//6. Dada la tupla (2,4,5,1,2,3,3.1416,23) regresa el numero 3.1416 
+val tuplaPi = (2,4,5,1,2,3,3.1416,23)
+println(tuplaPi._7)
+
+
---
+++ practica2.scala
@@ -0,0 +1,12 @@
+//5.-
+val lista2 = Set(1,3,3,4,6,7,3,7)
+val lista2 = collection.mutable.Set(1,3,4,6,7)
+
+//6.-
+val mapa = collection.mutable.Map(("Jose", 20), ("Luis", 24), ("Ana", 23), ("Susana", 27))
+
+//6 a.-
+mapa.keys
+
+//7 b.-
+mapa += ("Miguel" -> 23)
