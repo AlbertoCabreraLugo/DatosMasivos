9a47c96c7a9f91d07d6bd6360a12c4131ec9d020
RamonAlejandroArredondo <ramonalejandro3096@gmail.com>
2020-10-16 21:16:54 -0700

    Excercises 4 and 5

--- fibonacci.scala
+++ fibonacci.scala
@@ -41,3 +41,52 @@ println(f2)
 val f3 = fib3(7)
 println(f3)
 
+//4 Algorithm 4 Iterative version, 2 variables
+def fib4(n:Int) : Int = { 
+    var a = 0 
+    var b = 1 
+
+    for(i <- 1 to n)
+    { 
+        b = b + a 
+        a = b - a 
+    }
+    return a
+}
+
+//5 Algorithm 5 Iterative vector version
+def fib5(n:Int) : Int = {
+    var vector = new Array[Int](n+2)
+    if(n<2){
+        return n
+    }
+    else{
+        for(i <- 1 to n+1){
+            vector(i) = i
+        }
+
+        for(i <- 2 to n){
+            vector(i) = vector(i-1) + vector(i-2)
+        }
+        return vector(n)
+    }
+}
+
+//Printing of values to confirm algorithm
+
+val f1 = fib1(n=7)
+println(f1)
+
+val f2 = fib2(7)
+println(f2)
+
+
+val f3 = fib3(7)
+println(f3)
+
+val f4 = fib4(7)
+println(f4)
+
+val f5 = fib5(7)
+println(f5)
+
