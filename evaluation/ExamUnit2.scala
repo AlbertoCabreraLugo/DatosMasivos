c6668d98badbb531b9f6efa6f1356330da4bfa59
RamonAlejandroArredondo <71475909+RamonAlejandroArredondo@users.noreply.github.com>
2020-12-15 19:04:32 -0800

    Update ExamUnit2.scala
    Exercises from 6 to 8

--- evaluation/ExamUnit2.scala
+++ evaluation/ExamUnit2.scala
@@ -25,3 +25,46 @@ val dataClean = data.na.drop()
 
 //A vector is declared that transforms the data to the variable "features"n vector que se transforma los datos a la variable "features"
 val vectorFeatures = (new VectorAssembler().setInputCols(Array("sepal_length","sepal_width", "petal_length","petal_width")).setOutputCol("features"))
+
+// 6.- The features are transformed using the DataFrame
+val features = vectorFeatures.transform(dataClean)
+
+// We declare a "StringIndexer" which will transform the data in to "species" numeric values 
+val speciesIndexer = new StringIndexer().setInputCol("species").setOutputCol("label")
+
+// We adjust the indexed species with the vector features
+val dataIndexed = speciesIndexer.fit(features).transform(features)
+
+// With the variable "splits" we make a random cut
+val splits = dataIndexed.randomSplit(Array(0.6, 0.4), seed = 1234L)
+
+// The variable "train" is declared which will have 60% of the data
+val train = splits(0)
+
+// The variable "test" is declared which will have 40% of the data
+val test = splits(1)
+
+// 7.- Layer settings are established for the artificial neural network model
+val layers = Array[Int](4, 2, 2, 3)
+
+// The Multilayer algorithm trainer is configured with its respective parameters
+val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setBlockSize(128).setSeed(1234L).setMaxIter(100)
+
+// The model is trained with the training data
+val model = trainer.fit(train)
+
+// The model is tested and already trained
+val result = model.transform(test)
+
+// The prediction and the label that will be saved in the variable are selected
+val predictionAndLabels = result.select("prediction", "label")
+
+// Some data is shown
+predictionAndLabels.show()
+
+// The model precision estimate is run
+val evaluator = new MulticlassClassificationEvaluator().setLabelCol("label").setPredictionCol("prediction").setMetricName("accuracy")
+val accuracy = evaluator.evaluate(predictionAndLabels)
+
+// 8.- Error model is printed
+println(s"Test Error = ${(1.0 - accuracy)}")
